b4ac71aabf8c2addde7d539ba34cfbbfa1b43467
Jarom Bridges <jarombridges@hotmail.com>
2019-02-14 23:01:18 -0700

    initial keylogger commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,65 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+state = {
+  text: 'Have you typed yet?'
+}
+
+mirrorText = (event) => this.setState({text: event.target.value})
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Enter text here: <input type="text" onChange={this.mirrorText}/>
+        <br/>
+        Your text mirrored here:<p>{this.state.text}</p> 
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
