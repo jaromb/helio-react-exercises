bc9561877cd3f4a7f9877b3b44704784dc72b686
Jarom Bridges <jarombridges@hotmail.com>
2019-02-15 20:59:55 -0700

    codelist array to list commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { checkServerIdentity } from 'tls';
+import { ENGINE_METHOD_NONE } from 'constants';
 
 class App extends Component {
 state = {
   text: 'Have you typed yet?'
 }
 
+cities = ['Bakersfield, CA', 'Sandy, UT', 'Orem, UT', 'Strasbourg, France', 'Geneva, Switzerland', 'Lyon, France', 'Provo, UT', 'Salt Lake City, UT', 'Santa Cruz, CA']
+listItems = this.cities.map((city) =>
+  <li>{city}</li>
+);
+
+
+
+
 mirrorText = (event) => this.setState({text: event.target.value})
 
   render() {
     return (
       <div className="App">
-        Enter text here: <input type="text" onChange={this.mirrorText}/>
-        <br/>
-        Your text mirrored here:<p>{this.state.text}</p> 
+        <div class="keylogger">
+          Enter text here: <input type="text" onChange={this.mirrorText}/>
+          <br/>
+          Your text mirrored here:<p>{this.state.text}</p> 
+        </div>
+        <div class="array-list">
+          <ul>Some cities I've lived in:
+            {this.listItems}
+          </ul>
+        </div>
       </div>
     );
   }
