dcb2002eec177c1f296401d0a67cb5cecce1731b
Jarom Bridges <jarombridges@hotmail.com>
2019-02-24 22:12:07 -0700

    added list-search functionality

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ state = {
   num2: '',
   sum: ' ______',
   listText: '',
-  list: ['Bakersfield, CA', 'Sandy, UT', 'Orem, UT', 'Strasbourg, France', 'Geneva, Switzerland', 'Lyon, France', 'Provo, UT', 'Salt Lake City, UT', 'Santa Cruz, CA']
+  list: ['Bakersfield, CA', 'Sandy, UT', 'Orem, UT', 'Strasbourg, France', 'Geneva, Switzerland', 'Lyon, France', 'Provo, UT', 'Salt Lake City, UT', 'Santa Cruz, CA'],
+  filteredList: []
 }
 
 //KEYLOGGER
@@ -21,32 +22,29 @@ mirrorText = (event) => this.setState({text: event.target.value});
 
 //CITY LIST
 
-
-listItems =this.state.list.map((item) => 
-  <li>{item}</li>)
-
-
-updateListText = (event) => {this.setState({listText: event.target.value})
+updateListText = (event) => {
+  this.setState({listText: event.target.value, filteredList: this.state.list.filter((city) => { return city.includes(event.target.value) })})
 };
 
 handleEnter = (event) => {
   if(event.key === 'Enter') {
-    this.addToList()
+    this.addToList();
+    this.clearField();
   }
 }
 
 addToList = () => this.setState({
-  list: [...this.state.list, this.state.listText] }
+  list: [...this.state.list, this.state.listText], filteredList: [...this.state.filteredList, this.state.listText], listText: ''  }
 );
 
-// clearField = () => {
-//   this.refs.cityField = '';
-// }
+ clearField = () => {
+   this.refs.listField.value = '';
+ }
 
-// addToListAndClear = () => {
-//   this.addToList();
-//   this.clearField()
-// }
+ addToListAndClear = () => {
+   this.addToList();
+   this.clearField()
+ }
 
 //CALCULATOR - add two numbers
 
@@ -80,24 +78,27 @@ handleChangeInput2 = (e) => {
         
         <div id="array-list" class="newExercise">
           <h2>List from Array</h2>
-          <input type='text' ref='city-field' onChange={this.updateListText} onKeyPress={this.handleEnter}></input>
-          <button onClick={this.addToList}>Add to list</button>
-          <ul>Some cities I've lived in:
-            {this.state.list.map((item) => 
-   <li>{item}</li>)}
+          <input type='text' ref='listField' onChange={this.updateListText} onKeyPress={this.handleEnter}></input>
+          <button onClick={this.addToListAndClear}>Add to list</button>
+          <ul>Some cities I've lived in: 
+            <br/>
+            {(this.state.listText==='' || this.state.listText===null) ? (this.state.list.map((item) => 
+   <li>{item}</li>)) :
+               (this.state.filteredList.length) ? this.state.filteredList.map((city) => { return ( <li>{city}</li> ) }) : 
+               <li>No Results</li>
+            }
+            
           </ul>
         </div>
         
         <div id="add-numbers" class="newExercise">
-          <h2>Calculator</h2>
-          <label>Add two numbers:</label>
+          <h2>Add Two Numbers</h2>
+            Input 1:<input id="number1" type="number" onChange={this.handleChangeInput1}></input>
             <br/>
-              Input 1:<input id="number1" type="number" onChange={this.handleChangeInput1}></input>
-              <br/>
-              Input 2:<input id="number2" type="number" onChange={this.handleChangeInput2}></input>
-              <br/>
-              <button onClick={this.addTwoNumbers}>Add numbers</button>
-              <p>Sum ={this.state.sum}</p>
+            Input 2:<input id="number2" type="number" onChange={this.handleChangeInput2}></input>
+            <br/>
+            <button onClick={this.addTwoNumbers}>Add numbers</button>
+            <p>Sum ={this.state.sum}</p>
         </div>
       </div>
     );
